4c99e7631689dc1e36ea84231e9e469de3140422
Oluwatayoh <ojotayoh2@gmail.com>
2022-01-26 17:49:05 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'https://circularfx-api.vercel.app/api/',
-  baseUrl:'http://localhost:3000/api/'
+  baseUrl: 'https://circularfx-api.vercel.app/api/',
+  // baseUrl:'http://localhost:3000/api/'
 };
 
 /*
